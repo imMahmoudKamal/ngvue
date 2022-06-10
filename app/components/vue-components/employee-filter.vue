9843b975164b78eb4518137b5e7f4310a227f42f
[immahmoudkamal] <[immahmoudkamal@gmail.com]>
2022-06-10 08:16:44 +0200

    Req #1 - Sales team table filter

---
+++ app/components/vue-components/employee-filter.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="c-search">
+    <input class="c-search__input" type="search" v-model="search" placeholder="Search...">
+    <button class="c-button" @click="clear()">Clear</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['initialValue'],
+  
+  data() {
+    return {
+      search: ''
+    }
+  },
+
+  mounted: function() {
+    this.search = this.$props.initialValue;
+  },
+
+  watch: {
+    search() {
+      this.$emit('search', this.search)
+    }
+  },
+
+  methods: {
+    clear() {
+      this.search = '';
+    }
+  }
+};
+</script>
--- app/pages/home-page/home-page.controller.js
+++ app/pages/home-page/home-page.controller.js
@@ -1,10 +1,12 @@
 angular
   .module('appModule')
-  .controller('homeController', homePageController);
+  .controller('homeController', homePageController)
+  .filter('highlight', highlightFilter);
 
-function homePageController(Employees) {
+function homePageController(Employees, $location) {
   const homePageVm = this;
   homePageVm.employees = [];
+  homePageVm.searchInput = $location.$$search.filter;
 
   activate();
 
@@ -14,4 +16,22 @@ function homePageController(Employees) {
         homePageVm.employees = homePageVm.employees.concat(data.employees);
       });
   }
+
+  homePageVm.handleSearchEvent = function (input) {
+    homePageVm.searchInput = input;
+
+    if (input) {
+      $location.path('/').search({ filter: input });
+    } else {
+      $location.url($location.path());
+    }
+  };
+}
+
+function highlightFilter($sce) {
+  return function (text, phrase) {
+    if (phrase) text = text.replace(new RegExp('(' + phrase + ')', 'gi'), '<mark>$1</mark>');
+
+    return $sce.trustAsHtml(text);
+  };
 }
